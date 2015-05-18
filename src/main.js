5788bb1eddf37b56368bd1c2e18de14f6311fba1
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-05-17 23:32:54 -0500

    Safety push Sun May 17th

--- src/main.js
+++ src/main.js
@@ -3,23 +3,62 @@ var drawApi = "http://deckofcardsapi.com/api/draw/"
 //<<deck_id>>/?count=2"
 var API_PROXY = 'https://jsonp.afeld.me/?url=';
 var API_URL = "http://deckofcardsapi.com/api";
-var draw = $(".draw")
+var draw = $(".draw");
+var start = $(".start");
+var playerScore = [];
+var houseScore = [];
+var playerScoreTotal = 0;
+var houseScoreTotal = 0;
+
+//Main Problems.  Make face cards = 10.
+//Option 1:  Make face cards = 10 before they get into the score arrays
+//Option 2:  Leave face cards alone but use a for loop to go through the array and for each face card value add 10 to the scoreTotal
+//Deal with aces after that
+//then make end game states
+//
+//var playerScoreTotal = playerScore.reduce(function(previousValue, currentValue) {
+//  return previousValue + currentValue;
+//}
+
 
 //"shuffles 6 decks, outputs data to drawcard/s"
-$(".start").on("click", function startgame(){
+start.on("click", function startgame(){
   getJSON(shuffleApi, function (data) {
   drawCards(data);
   });
+
+//drawCards makes the cards appear on the page and passes values to the score arrays
+
   function drawCards(data) {
     getJSON(drawApi + data.deck_id + "/?count=4", function (cardData) {
    console.log(cardData);
    console.log(cardData.cards[0]);
+   //creating score arrays  // but parses NaN if a face card...
+   playerScore = playerScore.concat(parseInt(cardData.cards[0].value));
+   playerScore = playerScore.concat(parseInt(cardData.cards[1].value));
+   houseScore = houseScore.concat(parseInt(cardData.cards[2].value));
+   houseScore = houseScore.concat(parseInt(cardData.cards[3].value));
+   //totals the score arrays
+   playerScoreTotal = playerScore.reduce(function(prev, curr){
+    return prev + curr;
+      });
+   houseScoreTotal = houseScore.reduce(function(prev, curr){
+    return prev + curr;
+      });
+
+
+   //Lucas inspired code below for dealing with face values.  Doesnt quite work right now.  Just makes playerScoreTotal === 20
+   //if(cardData.cards[0].value === "King" || "Queen" || "Jack") {
+         //playerScoreTotal += 10;
+   //}
+   //if(cardData.cards[1].value === "King" || "Queen" || "Jack") {
+     //playerScoreTotal += 10;
+   //}
+   ////End Lucas inspiration
      appendPlayerCard(cardData);
      appendDealerCard(cardData);
-
-
     });
-  };
+      };
 
 function getJSON(url, cb) {
   JSONP_PROXY = 'https://jsonp.afeld.me/?url='
@@ -46,46 +85,22 @@ function appendPlayerCard(cardData){
   var $target=$("tr:last");
   $target.append("<td>" + "<img src=" + cardData.cards[0].image + "></img>" + "</td>");
   $target.append("<td>" + "<img src=" + cardData.cards[1].image + "></img>" + "</td>");
+  $target.append("<td>" + playerScoreTotal + "</td>");
 
-if(cardData.cards[0].value + cardData.cards[1].value === "21"){
+if(cardData.cards[0].value + cardData.cards[1].value <= 21){
   alert("Blackjack! Player Wins!");
       }
   })
   }
 function appendDealerCard(cardData){
- $(".dealerDraw").on("click", function() {
+ draw.on("click", function() {
   var $table=$("table");
   var $target=$("tr:last");
   $target.append("<td>" + "<img src=" + cardData.cards[2].image + "></img>" + "</td>");
   $target.append("<td>" + "<img class='cardBack' src=" + "http://i.imgur.com/xsqMd2z.jpg" + "></img>" + "</td>");
-
-  if(cardData.cards[2].value + cardData.cards[3].value === "21"){
+  if(cardData.cards[2].value + cardData.cards[3].value === 21){
   alert("Blackjack! Dealer Wins!");
       }
-
   })
  };
 });
-
-//$.get(API_PROXY + API_URL + "/shuffle/?deck_count=6", setDeckId, 'json');
-
-
-/*
-function addMovieDetail(data, id){
-  var $table=$(".table");
-  $table.append("<tr></tr>");
-  var $target=$("tr:last");
-  $target.attr("data-id", id);
-  $target.append("<td>" + data.Title + "</td>");
-  $target.append("<td>" + data.imdbRating + "</td>");
-  $target.append("<td>" + data.Year + "</td>");
-  $target.append("<td><img width='200' height='250' src=" + data.Poster + "></img></td>");
-  $target.append("<button class='delete'>Delete</button>");
-};
-
-$.get(FIREBASE_URL, function (movies) {
-  Object.keys(movies).forEach(function (id) {
-		addMovieDetail(movies[id], id);
-  });
-});
-*/
